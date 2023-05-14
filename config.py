49083d1669d0ad951f5d09612d9981b5f50628d2
sechabro <96841300+sechabro@users.noreply.github.com>
2023-05-13 20:13:57 -0400

    config correction for to_dir1

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 def directories():
     from_dir1 = str(os.getenv('DTOP', default=None))
-    from_dir1 = str(os.getenv('DOCS', default=None))
+    to_dir1 = str(os.getenv('DOCS', default=None))
     from_dir2 = str(os.getenv('DNLD', default=None))
-    return [from_dir1, from_dir1, from_dir2]
+    return [from_dir1, to_dir1, from_dir2]
