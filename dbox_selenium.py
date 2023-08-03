95463e4db14363fb6fb9b947d186ab782c7a0845
sechabro <96841300+sechabro@users.noreply.github.com>
2023-08-03 14:12:04 -0400

    selenium auth_token retrieval accomplished

--- dbox_selenium.py
+++ dbox_selenium.py
@@ -5,47 +5,55 @@ import time
 
 
 def get_auth_code(auth_url=None):
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    chrome_profile = "/Users/seanbrown/Library/Application\ Support/Google/Chrome/"
+    user_data = "Profile 1"
+    options.add_argument(f"--user-data-dir={chrome_profile}")
+    options.add_argument(f"--profile-directory={user_data}")
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(auth_url)
-    # print(auth_url)
-    driver.implicitly_wait(8)
-    # print(driver.current_url)
-    dbox_login(driver=driver)
+    time.sleep(3)
     code = auth_code(driver=driver)
     return code
 
 
 def dbox_login(driver=None):
+    '''in case login needs to occur beforehand, this can be
+    worked into the flow. current automation
+    relies on session info from chrome profile.'''
     driver.get("https://www.dropbox.com/login")
     email_input = driver.find_element(
         by=By.NAME, value="progressive_susi_email")
     driver.implicitly_wait(3)
     ActionChains(driver).send_keys_to_element(
-        email_input, "********").perform()
+        email_input, "email_here").perform()
     return_key = Keys.RETURN
     ActionChains(driver).send_keys_to_element(
         email_input, return_key).perform()
-    driver.implicitly_wait(3)
+    time.sleep(3)
     password_input = driver.find_element(
         by=By.NAME, value="login_password")
     ActionChains(driver).send_keys_to_element(
-        password_input, "********").perform()
+        password_input, "password_here").perform()
     print('found it')
     ActionChains(driver).send_keys_to_element(
         password_input, return_key).perform()
+    time.sleep(5)
     return
 
 
 def auth_code(driver=None):
+    time.sleep(3)
     continue_button = driver.find_element(
         by=By.ID, value="warning-button-continue")
-    continue_button.click()
-    driver.implicitly_wait(2)
+    ActionChains(driver).click(continue_button).perform()
+    time.sleep(2)
+    ActionChains(driver).click(continue_button).perform()
+    time.sleep(2)
     allow_button = driver.find_element(
-        by=By.CLASS_NAME, value="dig-Button dig-Button--primary dig-Button--large auth-button auth-button-allow")
-    allow_button.click()
-    driver.implicitly_wait(2)
-    auth_code_element = driver.find_element(
-        by=By.ID, value="auth-code-input")
-    auth_code = auth_code_element.value
+        by=By.CSS_SELECTOR, value="#auth > div.auth-buttons-container > button.dig-Button.dig-Button--primary.dig-Button--large.auth-button.auth-button-allow")
+    ActionChains(driver).click(allow_button).perform()
+    time.sleep(2)
+    auth_code_element = driver.find_element(by=By.ID, value="auth-code-input")
+    auth_code = auth_code_element.get_attribute("value")
     return auth_code
