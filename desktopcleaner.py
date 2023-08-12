66f6a571f7c8ea5f9aef9b61d5e34621d0c480b1
sechabro <96841300+sechabro@users.noreply.github.com>
2023-08-12 19:20:33 -0400

    file move process code consolidation

--- desktopcleaner.py
+++ desktopcleaner.py
@@ -1,6 +1,5 @@
 import os
 import re
-from datetime import datetime
 from datetime import date
 import config
 import dbox_oauth
@@ -8,28 +7,24 @@ import dbox_oauth
 dirs = config.directories()
 
 
-def date_mkdir(mod_date=None, item=None):
-    to_dir = dirs["to_dir"]
-    new_dir = os.path.join(to_dir, mod_date)
-    new_filepath = os.path.join(new_dir, item)
+# def file_move(directory=None, item=None):
+# to_dir = dirs["to_dir"]
+# new_dir = os.path.join(to_dir, mod_date)
+# new_filepath = os.path.join(directory, item)
 
-    if os.path.exists(new_dir):
-        filepath_retval = new_filepath
-        return filepath_retval
-    else:
-        os.mkdir(new_dir)
-        filepath_retval = new_filepath
-        return filepath_retval
-
-
-# def timestamp_format(time=None):
-#    string_time = str(time)
-#    mod_date = string_time[0:10]
-#    return mod_date
+# if os.path.exists(directory):
+#    filepath_retval = new_filepath
+#    return (directory, filepath_retval)
+# else:
+#    os.mkdir(directory)
+# filepath_retval = new_filepath
+# return filepath_retval
 
 
 def move_process(from_dir=None, upload_date=None):
     results = []
+    to_dir = dirs["to_dir"]
+    new_dir = os.path.join(to_dir, upload_date)
     for item in os.listdir(from_dir):
         filepath = os.path.join(from_dir, item)
         hidden = re.findall("^[.]", item)
@@ -39,14 +34,11 @@ def move_process(from_dir=None, upload_date=None):
         elif not os.path.isfile(filepath):
             continue
         else:
-            # metadata = os.stat((filepath))
-            # time = datetime.fromtimestamp(metadata.st_mtime)
-            # mod_date = timestamp_format(time=time)
-            new_filepath = date_mkdir(
-                mod_date=upload_date, item=item)
+            new_filepath = os.path.join(new_dir, item)
             os.rename(filepath, new_filepath)
             results.append(item)
-    return results
+    retval = (new_dir, results)
+    return retval
 
 
 def dir_search():
@@ -55,18 +47,18 @@ def dir_search():
     run_date = date.today()
     run_date_string = run_date.strftime("%Y_%\d_%m")
     for from_dir in from_dirs:
-        moved_item = move_process(
+        directory, moved_item = move_process(
             from_dir=from_dir, upload_date=run_date_string)
         results[from_dir] = moved_item
 
     print(f'files moved from: {results}')
+    return directory
 
 
 def main():
     try:
         access_token = dbox_oauth.oauth_flow()
-        filepath = None
-        dir_search()
+        filepath = dir_search()
         dbox_oauth.to_dropbox(filepath=filepath, access_token=access_token)
     except Exception:
         quit()
